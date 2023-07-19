776a0994f550a21ceb75b60eb224f95498b1ddf8
Erfan <erfan@Macs-MacBook-Pro.local>
2023-07-20 00:47:00 +0330

    Exclude README

--- src/tesseract_bot.py
+++ src/tesseract_bot.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-
 import logging
 import handler
 import argparse
@@ -10,49 +9,62 @@ from telegram.ext import Updater
 from telegram.ext import MessageHandler, filters
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-def resolve_args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-v', '--verbose', action='store_true')
-	args = parser.parse_args()
 
-	if args.verbose:
-		logging.basicConfig(level=logging.DEBUG,
-							format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def resolve_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        )
+
 
 def main():
-	resolve_args()
-	os.chdir(os.path.split(os.path.abspath(__file__))[0])
+    resolve_args()
+    os.chdir(os.path.split(os.path.abspath(__file__))[0])
 
-	try:
-	    os.mkdir(config.CACHE_DIR)
-	except OSError as e:
-	    if e.errno != errno.EEXIST:
-	        raise e
+    try:
+        os.mkdir(config.CACHE_DIR)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise e
 
-	app = ApplicationBuilder().token(config.BOT_TOKEN).build()
+    app = ApplicationBuilder().token(config.BOT_TOKEN).build()
 
-	app.add_handler(MessageHandler(filters.Document.MimeType("image/jpeg"), handler.Photo))
-	app.add_handler(MessageHandler(filters.Document.MimeType("image/png"), handler.Photo))
-	app.add_handler(MessageHandler(filters.Document.MimeType("image/jpg"), handler.Photo))
+    app.add_handler(
+        MessageHandler(filters.Document.MimeType("image/jpeg"), handler.Photo)
+    )
+    app.add_handler(
+        MessageHandler(filters.Document.MimeType("image/png"), handler.Photo)
+    )
+    app.add_handler(
+        MessageHandler(filters.Document.MimeType("image/jpg"), handler.Photo)
+    )
 
-	start_handler = CommandHandler('start', handler.start)
-	app.add_handler(start_handler)
+    start_handler = CommandHandler("start", handler.start)
+    app.add_handler(start_handler)
 
-	help_handler = CommandHandler('help', handler.help)
-	app.add_handler(help_handler)
+    help_handler = CommandHandler("help", handler.help)
+    app.add_handler(help_handler)
 
-	tesseract_handler = CommandHandler('tesseract', handler.tesseract)
-	app.add_handler(tesseract_handler)
+    tesseract_handler = CommandHandler("tesseract", handler.tesseract)
+    app.add_handler(tesseract_handler)
 
-	# message_handler = MessageHandler(filters._Photo, handler.message)
-	message_handler = MessageHandler(filters.Document.MimeType("image/jpeg"), handler.message)
-	app.add_handler(message_handler)
+    # message_handler = MessageHandler(filters._Photo, handler.message)
+    message_handler = MessageHandler(
+        filters.Document.MimeType("image/jpeg"), handler.message
+    )
+    app.add_handler(message_handler)
 
-	# unknown_handler = MessageHandler(filters.Command,handler.unknown)
-	unknown_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handler.unknown)
-	app.add_handler(unknown_handler)
+    # unknown_handler = MessageHandler(filters.Command,handler.unknown)
+    unknown_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handler.unknown)
+    app.add_handler(unknown_handler)
 
-	app.run_polling()
+    app.run_polling()
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
