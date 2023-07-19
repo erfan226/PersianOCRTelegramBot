63c2f0fd51bf537e8ae579d0d44fe9e68342ec0e
Erfan <erfan@Macs-MacBook-Pro.local>
2023-07-20 00:44:40 +0330

    Enhance image quality for better detection

--- src/handler.py
+++ src/handler.py
@@ -8,6 +8,7 @@ try:
 	from PIL import Image
 except ImportError:
 	from PIL import Image
+import re
 
 # pytesseract.pytesseract.Y = ( r'/usr/bin/tesseract' )
 intro = open("src/messageConfig/intro.txt", "r")
@@ -62,42 +63,46 @@ async def tesseract(update,context):
 	else:
 		await _photosize_to_parsed(context.bot, update, group_photos[update.message.chat_id])
 
-from io import BytesIO
-async def Photo(update,context): 
+# from io import BytesIO
+async def Photo(update, context): 
 	doc = await context.bot.get_file(update.message.document.file_id)
-	fileName=config.CACHE_DIR+'/photo_'+''.join(str(time.time()).split('.'))+'.png'
+	fileName = config.CACHE_DIR+'/photo_'+''.join(str(time.time()).split('.'))+'.png'
 	await doc.download_to_drive(fileName)
+	# img = Image.open(fileName).convert("L")
+	# img.thumbnail((img.width*2, img.height*2))
+	# print(img)
+	# print(img.height, img.width)
+	# scaled_img = config.CACHE_DIR+'/scaled_photo_'+''.join(str(time.time()).split('.'))+'.png'
+	# img.save(scaled_img)
 	try:
 		language = lang_dict.get(update.message.chat_id, DEFAULT_LANG)
-		image_text = pytesseract.image_to_string(Image.open(fileName), lang=language,config="--psm 3 --oem 1")
+		img = Image.open(fileName)
+		# The bigger the image, the better the output
+		if img.width < 1000:
+			s = 2
+		image_text = pytesseract.image_to_string(img.convert("L").resize((img.width * s, img.height * s)), lang=language, config="--psm 3 --oem 1")
 		if config.CACHE_TEMP:
 			os.remove(fileName)
-
-		sanitized_string = image_text
-		print(sanitized_string)
-
-		if sanitized_string:
-			response_msg =' {} \n'.format(sanitized_string)
+		if image_text:
+			response_msg =' {} \n'.format(image_text)
 		else:
 			f = open("messageConfig/notFound.txt", "r")
 			response_msg = f.read()
 			## response_msg = 'هیچ چیز پیدا نشد! :( \nParsed in {}'.format(available_langs[language])
-
 		await context.bot.sendMessage(chat_id=update.message.chat_id,
 			text=response_msg)
 	except Exception as e:
+		print(e)
 		# await context.bot.sendMessage(chat_id=update.message.chat_id, text=e)
 		await _something_wrong(update, context,e)
 
-async def _photosize_to_parsed(update,context, photosize):
+async def _photosize_to_parsed(update, context, photosize):
 	try:
 		filename = config.CACHE_DIR+'/photo_'+''.join(str(time.time()).split('.'))+'.png'
-
 		photosize.download(filename)
-
+		#TODO: ??
 		language = lang_dict.get(update.message.chat_id, DEFAULT_LANG)
-	
-		image_text = pytesseract.image_to_string(Image.open(filename), lang='bnazanin',config="--psm 3 --oem 1")
+		image_text = pytesseract.image_to_string(Image.open(filename), lang=language,config="--psm 3 --oem 1")
 
 		if config.CACHE_TEMP:
 			os.remove(filename)
@@ -116,6 +121,9 @@ async def _photosize_to_parsed(update,context, photosize):
 					parse_mode=telegram.ParseMode.MARKDOWN)
 	except Exception as e:
 		_something_wrong(update, context, e)
+
+# async def image_preprocess(img):
+	
 
 async def _something_wrong(update,context, e):
 	f = open("messageConfig/cautionError.txt", "r")
